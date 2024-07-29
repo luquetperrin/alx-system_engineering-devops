413427524f869bbb04c7fad0c339fd61c015bc30
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-07-29 10:59:05 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,17 +3,14 @@
 
 from json import dump
 from requests import get
-from sys import argv
 
 
 def fetch_json(url):
     """Fetch JSON data from a URL and handle potential errors."""
-    
     try:
         response = get(url)
         response.raise_for_status()  # Raises an HTTPError for bad responses
         return response.json()
-    
     except Exception as e:
         print(f"Error fetching data from {url}: {e}")
         return None
@@ -28,13 +25,15 @@ if __name__ == "__main__":
         exit(1)
 
     big_dict = {}
-    
+
     for user in users_result:
         todo_list = []
         user_id = user.get("id")
         username = user.get("username")
 
-        todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+        todos_url = (
+            f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+        )
         todo_result = fetch_json(todos_url)
 
         if todo_result is None:
