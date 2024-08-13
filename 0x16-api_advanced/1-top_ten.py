e6602e1472d695c11c31649b1f454c199161b305
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:23:00 -0700

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
+"""Queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit."""
 
-import requests
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit. If the subreddit is invalid, prints None."""
+    """Queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+    import requests
+    import sys
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    headers = {'User-Agent': 'My User Agent 1.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            for post in posts[:10]:
-                print(post.get('data', {}).get('title', ""))
-
-            if len(posts) == 0:
-                print("None")
-        except ValueError:
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
             print("None")
-    else:
+    except Exception:
         print("None")
