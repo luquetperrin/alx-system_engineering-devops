9851d476eb15d84e7de1c5b0e891289420dc2282
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:47:18 -0700

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,13 +10,17 @@ def recurse(subreddit, hot_list=[], after=None):
     
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
+    params = {'after': after, 'limit': 100}
 
     try:
         response = requests.get(url, headers=headers, params=params, allow_redirects=False)
         if response.status_code == 200:
             data = response.json().get('data', {})
             posts = data.get('children', [])
+            
+            if not posts and after is None:
+                return None
+            
             hot_list.extend([post.get('data', {}).get('title', "") for post in posts])
             
             after = data.get('after')
