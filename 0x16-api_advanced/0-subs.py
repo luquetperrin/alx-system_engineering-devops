bb70e3ab85bb0dd790d41f6b4ca4bfb2d99b14cc
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:41:15 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and returns the number of subscribers
-for a given subreddit."""
+
+"""
+Function that queries the Reddit API and returns the number of subscribers
+for a given subreddit.
+"""
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0."""
-    
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    usr = {"User-Agent": "custom-user-agent"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
+    response = requests.get(url, headers=usr, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        elif response.status_code == 404:
-            return 0
-        else:
-            return 0
-    except requests.RequestException:
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    except (KeyError, TypeError, ValueError):
         return 0
