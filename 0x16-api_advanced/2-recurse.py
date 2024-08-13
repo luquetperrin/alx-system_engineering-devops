5b8435d437700f8138dfc93177034e19dd588ea0
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:51:28 -0700

    Recursive

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,9 +4,12 @@ of titles of all hot articles for a given subreddit."""
 
 import requests
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=None, after=None):
     """Returns a list of titles of all hot articles for a given subreddit.
     If no results are found, returns None."""
+    
+    if hot_list is None:
+        hot_list = []
     
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -20,9 +23,9 @@ def recurse(subreddit, hot_list=[], after=None):
             
             if not posts and after is None:
                 return None
-            
+
             hot_list.extend([post.get('data', {}).get('title', "") for post in posts])
-            
+
             after = data.get('after')
             if after:
                 return recurse(subreddit, hot_list, after)
