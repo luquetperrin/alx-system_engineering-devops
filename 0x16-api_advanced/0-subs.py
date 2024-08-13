582ee995e76d5741ed827ed3c4026a96b18fb3ef
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:37:27 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers
+for a given subreddit."""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    for a given subreddit. Returns 0 for invalid subreddits."""
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    headers = {'User-Agent': 'My User Agent 1.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        try:
+    """Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0."""
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
             data = response.json()
             return data.get('data', {}).get('subscribers', 0)
-        except ValueError:
+        else:
             return 0
-    else:
+    except Exception:
         return 0
