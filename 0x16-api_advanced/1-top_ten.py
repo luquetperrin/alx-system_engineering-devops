96039c1c44202ecf3e2027d5ea9e5633831e6507
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:32:30 -0700

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,8 +5,8 @@ of the first 10 hot posts listed for a given subreddit."""
 import requests
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
+    """Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit. If the subreddit is invalid, prints None."""
     
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
