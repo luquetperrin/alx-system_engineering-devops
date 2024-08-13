97143cc83fdcae91269a0bd21690c65ab7381666
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-13 13:56:21 -0700

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+"""Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
 
 import requests
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit. If the subreddit is invalid, prints None."""
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
     
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
         if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
+            data = response.json().get('data', {})
+            posts = data.get('children', [])
+            
+            if not posts:
+                print("None")
+                return
             
             for post in posts[:10]:
                 print(post.get('data', {}).get('title', ""))
-                
-            if not posts:
-                print("None")
         else:
             print("None")
-    except Exception:
+    except requests.RequestException:
         print("None")
